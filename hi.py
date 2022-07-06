c1c6d3346613cf0ab691bd2c31499e5ca766d190
Grayson Donnelly <60803783+graydonnelly@users.noreply.github.com>
2022-07-06 11:01:19 -0500

    fixed code Andrew broke

--- hi.py
+++ hi.py
@@ -3,3 +3,5 @@ print('Bye')
 print('This is a bunch of code that I would not want to have to write in a stupid command line editor like vim or nano. Woohoo!')
 
 print('Grayson is cool')
+
+print('Andrew is cool')
