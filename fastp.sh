3101fcd141e728a731448b9f5e11a4a4bec42839
Grayson Donnelly <60803783+graydonnelly@users.noreply.github.com>
2022-07-15 16:22:56 -0500

    ready to run fastp

--- fastp.sh
+++ fastp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --account aDDLATER allocation requested
+#SBATCH --account p31752
 #SBATCH --partition normal
 #SBATCH --nodes=1
 #SABTCH --ntasks-per-node=20
@@ -14,25 +14,18 @@ module load anaconda3
 source activate paired_read
 source config.sh
 
-# One file
-# fastp -i ../raw_data/NCH0002R-M_S10_L001_R1_001.fastq.gz -I ../raw_data/NCH0002R-M_S10_L001_R2_001.fastq.gz -o out1.fastq.gz -O out2.fastq.gz
-
 # Iterate through all files
 for i in $(ls ../raw_data/dummy_files/*_R1_001.fastq.gz) # Iterate through R1
-#for i in $(ls ~/Desktop/dummy_files/*_R1_001.fastq.gz)
 do
-    i_basename=$(basename $i) # save
+    i_basename=$(basename $i) # Save basename
 	i_sub="${i_basename%%_*}" # Identifier (characters before underscore)
 	for j in $(ls ../raw_data/dummy_files/*_R2_001.fastq.gz) # Iterate through R2
-    #for i in $(ls ~/Desktop/dummy_files/*_R2_001.fastq.gz)
 	do
-		j_basename=$(basename $j) # save
+		j_basename=$(basename $j) # Save basename
 	    j_sub="${j_basename%%_*}" # Identifier (characters before underscore)
 		if [[ $i_sub == $j_sub ]] # Match reads via accession
 		then
-			#fastp -i ${i} -I ${j} --out1 ${i_sub}_fastp_out.R1.fastq.gz --out2 ${j_sub}_fastp_out.R2.fastq.gz --detect_adapter_for_pe --thread 16 --length_required 50
-            echo $i_sub
-            echo $j_sub
+			fastp -i ${i} -I ${j} --out1 ${i_sub}_fastp_out.R1.fastq.gz --out2 ${j_sub}_fastp_out.R2.fastq.gz --detect_adapter_for_pe --thread 16 --length_required 50
 		fi
 	done
 done
@@ -41,6 +34,6 @@ done
 conda deactivate
 
 # Make directory and move output files
-#mkdir ${fastpDir}
-#mv out* ${fastpDir}/
-#mv fastp.html fastp.json ${fastpDir}/
+mkdir ${fastpDir}
+mv out* ${fastpDir}/
+mv fastp.html fastp.json ${fastpDir}/
