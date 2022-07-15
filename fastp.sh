423851da3fe686ce727cc5f34b2e33198c51bcc4
Grayson Donnelly <60803783+graydonnelly@users.noreply.github.com>
2022-07-15 16:14:48 -0500

    test3

--- fastp.sh
+++ fastp.sh
@@ -18,12 +18,16 @@ source config.sh
 # fastp -i ../raw_data/NCH0002R-M_S10_L001_R1_001.fastq.gz -I ../raw_data/NCH0002R-M_S10_L001_R2_001.fastq.gz -o out1.fastq.gz -O out2.fastq.gz
 
 # Iterate through all files
-for i in $(ls ../raw_data/dummy_files/*_R1_001.fastq.gz) # Iterate through R1
+#for i in $(ls ../raw_data/dummy_files/*_R1_001.fastq.gz) # Iterate through R1
+for i in $(ls ~/Desktop/dummy_files/*_R1_001.fastq.gz)
 do
-	i_sub="${i%%_*}" # Identifier (characters before underscore)
-	for j in $(ls ../raw_data/dummy_files/*_R1_001.fastq.gz) # Iterate through R2
+    i_basename=$(basename $i) # save
+	i_sub="${i_basename%%_*}" # Identifier (characters before underscore)
+	#for j in $(ls ../raw_data/dummy_files/*_R2_001.fastq.gz) # Iterate through R2
+    for i in $(ls ~/Desktop/dummy_files/*_R2_001.fastq.gz)
 	do
-		j_sub="${j%%_*}" # Identifier (characters before underscore)
+		j_basename=$(basename $j) # save
+	    j_sub="${j_basename%%_*}" # Identifier (characters before underscore)
 		if [[ $i_sub == $j_sub ]] # Match reads via accession
 		then
 			#fastp -i ${i} -I ${j} --out1 ${i_sub}_fastp_out.R1.fastq.gz --out2 ${j_sub}_fastp_out.R2.fastq.gz --detect_adapter_for_pe --thread 16 --length_required 50
