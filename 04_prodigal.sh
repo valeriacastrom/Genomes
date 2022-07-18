e1bde189d97ec8761e9a7414c3a9547fadf70876
Andrew Watson <95504422+realandrewwatson@users.noreply.github.com>
2022-07-18 12:54:33 -0500

    made some files and stuff

--- 04_prodigal.sh
+++ 04_prodigal.sh
@@ -11,26 +11,16 @@
 #Setup
 module purge all
 module load anaconda3
+module load prodigal
 source config.sh
 
 #Prodigal
 cd ${spadesDir}
+
+
+
 prodigal -i contigs.fasta -o out.gene.coords.gbk -a out.protein.translations.faa
 
-#Egg-nog
-find_in_conda_env(){
-    conda env list | grep "${@}" >/dev/null 2>/dev/null
-}
-
-if find_in_conda_env "diamond" ; then
-    source activate diamond
-else 
-    conda create -n diamond -c bioconda -c conda-forge python=3.9 #Create environment with diamond
-    source activate diamond
-    conda install -c bioconda diamond #Install diamond
-fi
-
-emapper.py -i out.protein.translations.faa -o egg_nog -m diamond --cpu 20
 
 #Moves to prodigal directory
 mv out.gene* ${prodigalDir}/
