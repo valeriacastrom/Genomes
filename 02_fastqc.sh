291cd8844d79d4616c14537762b6c9af1ec3763a
Grayson Donnelly <60803783+graydonnelly@users.noreply.github.com>
2022-07-18 12:34:28 -0500

    fastqc worked, just have to run multiqc

--- 02_fastqc.sh
+++ 02_fastqc.sh
@@ -17,12 +17,11 @@ source activate paired_read
 source config.sh
 
 # FASTQC analysis
-fastqc -t 12 ${fastpDir}/*fastq.gz
+#fastqc -t 12 ${fastpDir}/*fastq.gz
 
 # To move FastQC output into new directory
-mkdir ${fastqcDir}
-mv *fastqc.html ${fastqcDir}
-mv *fastqc.zip ${fastqcDir}
+#mkdir ${fastqcDir}
+#mv ${fastpDir}/*fastqc.* ${fastqcDir}
 
 # MultiQC analysis
 multiqc ${fastqcDir}
