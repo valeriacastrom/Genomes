9909fa5c4940c8f3dc2abaeb99579787c7384dbd
Andrew Watson <95504422+realandrewwatson@users.noreply.github.com>
2022-07-18 14:18:44 -0500

    Update 05_eggnog.sh

--- 05_eggnog.sh
+++ 05_eggnog.sh
@@ -10,23 +10,35 @@
 
 #Setup
 module purge all
+module load eggnog-mapper
 module load anaconda3
 source config.sh
+source activate annotation
+
 
 #Egg-nog
 find_in_conda_env(){
     conda env list | grep "${@}" >/dev/null 2>/dev/null
 }
 
-if find_in_conda_env "diamond" ; then
-    source activate diamond
-else 
-    conda create -n diamond -c bioconda -c conda-forge python=3.9 #Create environment with diamond
-    source activate diamond
-    conda install -c bioconda diamond #Install diamond
-fi
+#check if diamond is installed in the 
+#if find_in_conda_env "diamond" ; then
+#    source activate diamond
+#else 
+#    conda create -n diamond -c bioconda -c conda-forge python=3.9 #Create environment with diamond
+#    source activate diamond
+#    conda install -c bioconda diamond #Install diamond
+#fi
+
+
+
+
 
 emapper.py -i out.protein.translations.faa -o egg_nog -m diamond --cpu 20
+
+
+#deactivate the conda environment containing diamond
+source deactivate
 
 #Moves to prodigal directory
 mv egg_nog* ${prodigalDir}
