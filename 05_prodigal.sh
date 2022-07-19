0111bffc2c265f8bd50af94918443fca283d64d6
Andrew Watson <95504422+realandrewwatson@users.noreply.github.com>
2022-07-19 11:46:35 -0500

    blah blah

--- 05_prodigal.sh
+++ 05_prodigal.sh
@@ -4,13 +4,14 @@
 #SBATCH --partition normal
 #SBATCH --nodes=1
 #SABTCH --ntasks-per-node=20
-#SBATCH --time=40:00:00
+#SBATCH --time=04:00:00
 #SBATCH --mem=100gb
 #SBATCH --job-name="annotation_gen_assembly"
 
 #Setup
 module purge all
 module load anaconda3
+module load prodigal 
 source config.sh
 
 #Prodigal
