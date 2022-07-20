15593b75254ea4ae97ca9a7394a2d084b3ec477a
Grayson Donnelly <60803783+graydonnelly@users.noreply.github.com>
2022-07-20 11:29:07 -0500

    deleted unnecessary files

--- hi.py
+++
@@ -1,8 +0,0 @@
-print('Bye')
-print('Vale')
-print('This is a bunch of code that I would not want to have to write in a stupid command line editor like vim or nano. Woohoo!')
-print('This is a bunch of code that I would not want to have to write in a stupid command line editor like vim or nano. Woohoo!')
-print('Grayson is cool')
-print('Andrew is cool')
-x = 'your mother'
-print(x)
