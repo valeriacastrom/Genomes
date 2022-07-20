809fcdfd15df861633dcf23770c456b031ee25bc
Andrew Watson <95504422+realandrewwatson@users.noreply.github.com>
2022-07-20 11:01:17 -0500

    Update 07_phylophlan.sh

---
+++ 07_phylophlan.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --account p31752
+#SBATCH --partition normal
+#SBATCH --nodes=1
+#SABTCH --ntasks-per-node=20
+#SBATCH --time=04:00:00
+#SBATCH --mem=100gb
+#SBATCH --job-name="prodigal_gen_assembly"
+#SBATCH --mail-type=all
+#SBATCH --mail-user=andrewwatson2025@u.northwestern.edu
+
