5f400b7f0550570533013da7e0cfff1ab1ecc58d
Valeria <valeriacastro2025@u.northwestern.edu>
2022-07-26 16:33:06 -0500

    Create 08_gtdbtk..sh

---
+++ 08_gtdbtk..sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --account p31752
+#SBATCH --partition normal
+#SBATCH --nodes=1
+#SABTCH --ntasks-per-node=20
+#SBATCH --time=40:00:00
+#SBATCH --mem=100gb
+#SBATCH --job-name="gtdbtk_gen_assembly"
+#SBATCH --mail-type=all
+#SBATCH --mail-user=andrewwatson2025@u.northwestern.edu
+
+module load gtdbtk
+
