b61e29ac3a495247d4ef0ca8e37ce425d33163a9
Valeria <valeriacastro2025@u.northwestern.edu>
2022-07-27 13:43:16 -0500

    changed time

--- 08_gtdbtk.sh
+++ 08_gtdbtk.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition normal
 #SBATCH --nodes=1
 #SABTCH --ntasks-per-node=20
-#SBATCH --time=40:00:00
+#SBATCH --time=10:00:00
 #SBATCH --mem=100gb
 #SBATCH --job-name="gtdbtk_gen_assembly"
 #SBATCH --mail-type=all
